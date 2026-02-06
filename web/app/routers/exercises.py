ecb27c9e4952ec949e75a6a6761d2f0efc1f9493
Patricia E. Miguel <contacto@patriciaemiguel.com>
2026-02-06 18:32:43 -0300

    feat: mostrar listado de ejercicios en html con htmx

--- web/app/routers/exercises.py
+++ web/app/routers/exercises.py
@@ -1,18 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from pathlib import Path
 import ast
 
 router = APIRouter()
+templates = Jinja2Templates(directory="web/templates")
 
 
 @router.get("/exercises")
-def list_exercises():
+def list_exercises(request: Request):
     base_path = Path("content/python/ESP/src")
-
     exercises = {}
 
     for file_path in base_path.glob("*.py"):
-        category = file_path.stem  # nombre del archivo sin .py
+        category = file_path.stem
 
         with open(file_path, "r", encoding="utf-8") as file:
             tree = ast.parse(file.read())
@@ -25,4 +27,15 @@ def list_exercises():
 
         exercises[category] = functions
 
+    # Si el request viene de HTMX, devuelve HTML
+    if request.headers.get("hx-request") == "true":
+        return templates.TemplateResponse(
+            "fragments/exercise_list.html",
+            {
+                "request": request,
+                "exercises": exercises,
+            },
+        )
+
+    # Caso contrario, devuelv JSON
     return exercises
--- web/main.py
+++ web/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+
 from web.app.routers.health import router as health_router
 from web.app.routers.exercises import router as exercises_router
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="web/templates")
+
 app.include_router(health_router)
 app.include_router(exercises_router)
+
+
+@app.get("/")
+def index(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request},
+    )
