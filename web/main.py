921537683145482ee7c013c402e4626a286a5084
Patricia E. Miguel <contacto@patriciaemiguel.com>
2026-02-06 20:22:05 -0300

    chore: agregar favicon y endpoint.

--- web/main.py
+++ web/main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
+from pathlib import Path
+from fastapi.responses import FileResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
+from fastapi.staticfiles import StaticFiles
 
 from web.app.routers.health import router as health_router
 from web.app.routers.exercises import router as exercises_router
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="web/templates")
+# Static files
+BASE_DIR = Path(__file__).resolve().parent
+
+app.mount(
+    "/static",
+    StaticFiles(directory=BASE_DIR / "static"),
+    name="static",
+)
+
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 app.include_router(health_router)
 app.include_router(exercises_router)
@@ -18,4 +30,10 @@ def index(request: Request):
     return templates.TemplateResponse(
         "index.html",
         {"request": request},
+    )
+
+@app.get("/favicon.ico")
+def favicon():
+    return FileResponse(
+        BASE_DIR / "static" / "favicon.ico"
     )
